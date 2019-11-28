61d0a0c7aeacdbe6f8ad49c58e3cd3c9343d9c75
Beatriz Fernandes <beatrizcf2@al.insper.edu.br>
2019-11-28 18:02:24 -0300

    est

---
+++ App/assets/js/editable.js
@@ -0,0 +1,26 @@
+let history = []
+
+
+function add(total) {
+
+	let prev = getValue(total)
+
+	let curr = prev + 1
+
+	setValue(total, curr)
+}
+
+function sub(total) {
+
+	let prev = getValue(total)
+
+	if(prev > 0) {
+
+		let curr = prev - 1
+
+		setValue(total, curr)
+
+	}
+}
+
+
